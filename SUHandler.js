13a606e054e0f9c9cd1214f7d496d7cccb70934a
Erik Rönmark <eriro331@student.liu.se>
2017-10-26 13:11:04 +0200

    v

--- SUHandler.js
+++ SUHandler.js
@@ -66,11 +66,6 @@ function handleFormSubmit(event) { // handles form submit withtout any jquery
     event.preventDefault(); // we are submitting via xhr below
     var data = getFormData(); // get the values submitted in the form
 
-    /* OPTION: Remove this comment to enable SPAM prevention, see README.md
-  if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
-    return false;
-  }
-  */
 
     var url = event.target.action; //
     var xhr = new XMLHttpRequest();
